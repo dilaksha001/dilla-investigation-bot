a984f8c9eada72cac4bcf578679204c2ec5abfbe
dilaksha001 <dilakshapremarathna001@gmail.com>
2026-02-23 15:01:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def perform_reverse_dns(ip_address):
     Returns the hostname if found, otherwise 'Unknown' or an error message.
     """
     try:
-        # socket.gethostbyaddr attempts to resolve an IP address to a hostname.
+        # socket.gethostbyaddr attempts to resolve an IP address to a hostname.
         hostname, _, _ = socket.gethostbyaddr(ip_address)
         return hostname
     except socket.herror:
